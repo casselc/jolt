9b648335f89f0e180c9716c82dadcf993eeb2d59
Chuck Cassel <619504+casselc@users.noreply.github.com>
2026-08-28 05:06:49 -0700

    feat(aspects): instrument fixed function entries

--- test/chez/aspect-build-app/src/app/target.clj
+++ test/chez/aspect-build-app/src/app/target.clj
@@ -5,3 +5,18 @@
   (if (= x "throw")
     (throw (ex-info "application failure" {:kind :application}))
     (str x "!")))
+
+(defn callback [x]
+  (println (str "callback " x))
+  (cond
+    (= x "recur") (recur "done")
+    (= x "throw") (throw (ex-info "callback failure" {:kind :callback}))
+    :else (str x "?")))
+
+(defn invoke-callback [f x]
+  ;; No resolved app.target/callback invocation exists for a call selector.
+  (f x))
+
+(defn numeric-callback [^long x]
+  (println (str "numeric-callback " x))
+  (inc x))
